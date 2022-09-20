4e6647e9be2e6f59cd2e63b95a3fd6da1d5c432e
Nikhil Bakshi <110008146+Nikhil1682@users.noreply.github.com>
2022-09-20 18:27:26 +0530

    Add files via upload

---
+++ Reverse_linkedlist(k-grps).cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+    Following is the Linked List node structure
+
+    class Node
+    {
+    public:
+        int data;
+        Node *next;
+        Node(int data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+
+Node* kReverse(Node* head, int k) {
+    // Write your code here.
+    if(head==NULL){
+        return NULL;
+    }
+    
+    Node* next=NULL;
+    Node* curr=head;
+    Node* prev=NULL;
+    int cnt=0;
+    while(curr!=NULL && cnt<k){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+        cnt++;
+    }
+    //recursion call
+    if(next!=NULL){
+        head->next=kReverse(next,k);
+    }
+    return prev; //head
+    
+    
+}
